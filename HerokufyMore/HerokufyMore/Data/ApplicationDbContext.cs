4f35ca24dd337baf6934becf61c3b89ed6a148c8
Kyungrae Kim <kyungraekim@outlook.com>
2020-10-11 16:53:09 -0700

    application user db context added

---
+++ HerokufyMore/HerokufyMore/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using HerokufyMore.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HerokufyMore.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ HerokufyMore/HerokufyMore/Models/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HerokufyMore.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public static class ApplicationRoles
+    {
+        public const string Admin = "Administrator";
+        public const string Member = "Member";
+    }
+}
