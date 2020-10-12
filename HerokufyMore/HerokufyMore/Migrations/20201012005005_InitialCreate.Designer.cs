49e0cfde1110f9b44c8e57484b631e4ab8b53b54
Kyungrae Kim <kyungraekim@outlook.com>
2020-10-11 17:50:43 -0700

    Dockerfile and workflow updated with new arguments

--- HerokufyMore/HerokufyMore/Migrations/20201012005005_InitialCreate.Designer.cs
+++ HerokufyMore/HerokufyMore/Migrations/20201012005005_InitialCreate.Designer.cs
@@ -10,7 +10,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace HerokufyMore.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20201012003906_InitialCreate")]
+    [Migration("20201012005005_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
