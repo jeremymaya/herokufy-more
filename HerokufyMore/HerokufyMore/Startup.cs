d57518cdd07002bf7af0008c9cf0047d809c4f2d
Kyungrae Kim <kyungraekim@outlook.com>
2020-10-11 17:58:40 -0700

    startup updated

--- HerokufyMore/HerokufyMore/Startup.cs
+++ HerokufyMore/HerokufyMore/Startup.cs
@@ -107,7 +107,7 @@ namespace HerokufyMore
 
             var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
 
-            RoleInitializer.SeedData(serviceProvider, userManager, Configuration, env);
+            RoleInitializer.SeedData(serviceProvider, userManager, Configuration, WebHostEnvironment);
 
             // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger();
